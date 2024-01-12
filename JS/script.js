22f07ac159ed7d7c59fb82c2f89b0975cecf7c31
emanuelaraujocaceres <134813326+emanuelaraujocaceres@users.noreply.github.com>
2024-01-12 09:22:25 -0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Botão Início
+    document.getElementById('btnInicio').addEventListener('click', function () {
+        window.location.href = 'C:/Users/Administrator/Desktop/fenix/index.html';
+    });
+
+    // Botão Tratamentos
+    document.getElementById('btnTratamentos').addEventListener('click', function () {
+        window.location.href = 'C:/Users/Administrator/Desktop/fenix/tratamentos.html';
+    });
+
+    // Botão Convênios
+    document.getElementById('btnConvenios').addEventListener('click', function () {
+        window.location.href = 'C:/Users/Administrator/Desktop/fenix/convenios.html';
+    });
+
+    // Botão Contato
+    document.getElementById('btnContato').addEventListener('click', function () {
+        window.location.href = 'C:/Users/Administrator/Desktop/fenix/contato.html';
+    });
+});
+
